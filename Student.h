98c468ba23121624c14bb44195b0ceefea4f5485
marinamalak <108700277+marinamalak@users.noreply.github.com>
2023-02-15 21:01:49 +0200

    Delete Student.h

--- Student.h
+++
@@ -1,8 +0,0 @@
-#ifndef _STUDENT_H
-#define _STUDENT_H
-#include<stdlib.h>
-void Student_detailes();
-void check_order_user();
-void Change_Password();
-void Student_detailes();
-#endif // _STUDENT_H
