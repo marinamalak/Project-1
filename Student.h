d013de24dddda5843336ead41084a27abc8ce659
IbrahemAshour <100946510+IbrahemAshour@users.noreply.github.com>
2023-02-15 03:33:55 +0200

    Add files via upload

---
+++ Student.h
@@ -0,0 +1,8 @@
+#ifndef _STUDENT_H
+#define _STUDENT_H
+#include<stdlib.h>
+void Student_detailes();
+void check_order_user();
+void Change_Password();
+void Student_detailes();
+#endif // _STUDENT_H
